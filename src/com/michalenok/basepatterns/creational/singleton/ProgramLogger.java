41c5b21d8c7c803c0b91a7487c77d433cf8f6932
Michalenok Marina <mihalenokee@outlook.com>
2023-10-10 12:32:17 +0300

    feat: creational pattern singleton

---
+++ src/com/michalenok/basepatterns/creational/singleton/ProgramLogger.java
@@ -0,0 +1,25 @@
+package com.michalenok.basepatterns.creational.singleton;
+
+public class ProgramLogger {
+    private static volatile ProgramLogger programLogger;
+    private static String logFile = "This is log file. \n\n";
+    private ProgramLogger(){
+
+    }
+    public static ProgramLogger getProgramLogger() {
+        if (programLogger == null){
+            synchronized (ProgramLogger.class){
+                if (programLogger == null){
+                    programLogger = new ProgramLogger();
+                }
+            }
+        }
+        return programLogger;
+    }
+    public void addLogInfo(String logInfo){
+        logFile += logInfo + "\n";
+    }
+    public void showLogFile(){
+        System.out.println(logFile);
+    }
+}
